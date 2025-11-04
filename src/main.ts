9e8ebee8cb4841ad547a4bc606df7c214688dc51
eatornquist <eatornquist04@gmail.com>
2025-11-03 22:14:51 -0500

    Add CORS configuration in main.ts for improved API security and flexibility

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(process.env.PORT ?? 3001);
 }
